84453b1ad92e31e5895c75cbddc32bc05fb83296
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-29 18:12:46 +0530

    give type to functions as params - callback functions

---
+++ 05 CallBack-Function/main.js
@@ -0,0 +1,22 @@
+"use strict";
+// How to give the type of the function
+const runFunction_1 = (fn) => {
+    setTimeout(fn, 2000);
+};
+const runFunction_2 = (fn) => {
+    setTimeout(() => {
+        const result = fn();
+        console.log(result); // Handle the result of the number function
+    }, 5000);
+};
+const greet = () => {
+    console.log("Hello there!!");
+    // as it returns nothing so its a void
+};
+const add = (a, b) => {
+    return a + b;
+};
+// Pass the function itself, not the result of invoking it.
+runFunction_1(greet);
+// Wrap the add function call in another function that matches the expected signature.
+runFunction_2(() => add(5, 5));
