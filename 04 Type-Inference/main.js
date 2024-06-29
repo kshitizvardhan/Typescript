dc39814226e9221eaf85e1dd7dc312de95e687bd
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-29 17:56:32 +0530

    Type Inference

---
+++ 04 Type-Inference/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// type inference- the compiler knows the returnType to be a boolean implicitly, so we didnt write it explicitly.
+// But Usually a good practice to write it.
+const isLegal = (age) => {
+    if (age >= 18) {
+        return true;
+    }
+    else
+        return false;
+};
+console.log(isLegal(19));
+/*
+
+const isLegal = (age:number):boolean => {
+    if(age >= 18){
+        return true;
+    } else return false;
+}
+
+*/ 
