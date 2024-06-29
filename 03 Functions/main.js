de5170fcf97c7dca1120732710fd07711e5d17e1
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-29 17:49:21 +0530

    functions with types and return type

---
+++ 03 Functions/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+const value = add(5, 5);
+console.log(value);
---
+++ 03 Functions/main.ts
@@ -0,0 +1,6 @@
+function add(a:number, b:number):number {
+    return a+b;
+}
+
+const value = add(5,5);
+console.log(value);
